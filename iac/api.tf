8324ddd2a3435bd4eff78d7ca7aa1a5c71966af1
Escalante Díaz Rodrigo Alonso <rescalanted1@upao.edu.pe>
2026-04-17 01:57:44 +0000

    feature: añadir configuraciones de red

--- iac/api.tf
+++ iac/api.tf
@@ -1,9 +1,11 @@
 resource "docker_container" "api" {
-  name  = "api-${terraform.workspace}-01"
+  name  = "api-${terraform.workspace}"
   image = "lab/api"
-
-   ports {
-    internal = "3000"
+  networks_advanced { name = docker_network.app_network.name }
+  ports {
+    internal = 3000
     external = var.api_port[terraform.workspace]
   }
+  # Para que la API sepa dónde está la BD
+  env = [ "DB_HOST=bd-${terraform.workspace}" ]
 }
--- iac/db.tf
+++ iac/db.tf
@@ -1,9 +1,23 @@
-resource "docker_container" "api" {
-  name  = "api-${terraform.workspace}-01"
-  image = "lab/api"
+resource "docker_container" "db" {
+  name  = "db-${terraform.workspace}"
+  image = "postgres:15-alpine"
+  
+  # Conexión a la red segura
+  networks_advanced {
+    name = docker_network.app_network.name
+    aliases = ["db-host"] 
+  }
 
-   ports {
-    internal = "3000"
-    external = var.api_port[terraform.workspace]
+  # Variables de entorno para inicializar la BD
+  env = [
+    "POSTGRES_USER=myuser",
+    "POSTGRES_PASSWORD=mypassword",
+    "POSTGRES_DB=mydb"
+  ]
+
+  # Mapeo de puertos según el workspace
+  ports {
+    internal = 5432
+    external = var.db_port[terraform.workspace]
   }
 }
