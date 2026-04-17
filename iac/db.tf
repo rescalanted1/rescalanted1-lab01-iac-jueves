3e69d6b5c967fa558db50fd48980ed8dacf5818d
Escalante Díaz Rodrigo Alonso <rescalanted1@upao.edu.pe>
2026-04-17 01:50:37 +0000

    chore: crear y configurar dockerfile de la db

---
+++ iac/db.tf
@@ -0,0 +1,9 @@
+resource "docker_container" "api" {
+  name  = "api-${terraform.workspace}-01"
+  image = "lab/api"
+
+   ports {
+    internal = "3000"
+    external = var.api_port[terraform.workspace]
+  }
+}
